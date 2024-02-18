3aa9ca0ce6ceb06ae5efd796c28261f2dd156fec
Bryant1376 <160219603+Bryant1376@users.noreply.github.com>
2024-02-18 14:17:43 -0500

    Add files via upload
    First submission

---
+++ P1LAB1_BryantKeyon.py
@@ -0,0 +1,13 @@
+# Your Name
+# Date
+# Assignment Name
+# A brief description of the project
+
+# Program to output a welcome message based on user input
+
+# Get user input for first name
+first_name = input("Enter your first name: ")
+
+# Display welcome message
+print(f"Hey {first_name}")
+print("Welcome to zyBooks")
