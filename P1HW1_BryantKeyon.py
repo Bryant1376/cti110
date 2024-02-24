92e1acdaacd833b9d81a09a0f2b12b1992aaada3
Bryant1376 <160219603+Bryant1376@users.noreply.github.com>
2024-02-24 00:06:38 -0500

    P1HW1_BryantKeyon.py

--- P1HW1_BryantKeyon.py
+++ P1HW1_BryantKeyon.py
@@ -3,6 +3,13 @@
 # P1HW1
 #Description: Basic output with variables
 
+#Use the input function to get an integer from the user. Assign the integer to a variable
+#Print the integer, the square of the integer, and the cube of the integer, each on a separate line
+#Use the input function to get another integer from the user. Assign it to a variable
+#Print both integers separated by a plus sign and then the sum of the integers
+#Print both integers separated by a multiplication sign and then the product of the integers
+#Do NOT hardcode the integers. The values should be able to change each time the program runs.
+
 # Get an integer from the user and assign it to a variable
 num1 = int(input("Enter an integer: "))
 
