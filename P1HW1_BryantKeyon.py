d337077b66d672a7c3c42140350d70ccdce263ed
Bryant1376 <160219603+Bryant1376@users.noreply.github.com>
2024-02-23 23:51:13 -0500

    P1HW1_BryantKeyon.py

--- P1HW1_BryantKeyon.py
+++ P1HW1_BryantKeyon.py
@@ -1,19 +1,19 @@
-# Your Name
-# Date
-# Assignment Name
-# A brief description of the project
+# Keyon Bryant
+# 2/23/24
+# P1HW1
+#Description: Basic output with variables
 
-
+# Get an integer from the user and assign it to a variable
 num1 = int(input("Enter an integer: "))
 
-
+# Print the integer, the square, and the cube of the integer
 print(f"The integer is: {num1}")
 print(f"The square of the integer is: {num1 ** 2}")
 print(f"The cube of the integer is: {num1 ** 3}")
 
-
+# Get another integer from the user and assign it to a variable
 num2 = int(input("Enter another integer: "))
 
-
+# Print the sum and product of the two integers
 print(f"{num1} + {num2} = {num1 + num2}")
 print(f"{num1} * {num2} = {num1 * num2}")
