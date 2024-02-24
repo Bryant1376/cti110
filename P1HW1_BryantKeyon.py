159a5f95547b00734dff8f03ceb06fea4aced718
Bryant1376 <160219603+Bryant1376@users.noreply.github.com>
2024-02-23 23:04:25 -0500

    Add files via upload
    P1HW1

---
+++ P1HW1_BryantKeyon.py
@@ -0,0 +1,19 @@
+# Your Name
+# Date
+# Assignment Name
+# A brief description of the project
+
+
+num1 = int(input("Enter an integer: "))
+
+
+print(f"The integer is: {num1}")
+print(f"The square of the integer is: {num1 ** 2}")
+print(f"The cube of the integer is: {num1 ** 3}")
+
+
+num2 = int(input("Enter another integer: "))
+
+
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
